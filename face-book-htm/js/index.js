e4989069f96489b147c8a5d87c45b0df0951e523
ChandanOffice-git <chandantiwari1996ct@gmail.com>
2025-01-05 00:31:13 +0530

    first commit

--- face-book-htm/js/index.js
+++ face-book-htm/js/index.js
@@ -16,3 +16,23 @@ darkBtn.onclick = function(){
     document.body.classList.toggle("dark-mood")
 }
 
+
+
+// ......header2.........//
+var account_infoI = document.querySelector(".account-infoI");
+
+
+function account_infoToggleI() {
+    account_infoI.classList.toggle("account-info-heightI");
+    
+};
+
+// --------darkmood2 header-----------//
+var darkBtn = document.getElementById("dark-btnII");
+
+darkBtn.onclick = function(){
+    darkBtn.classList.toggle("dark-btn-onII");
+    document.body.classList.toggle("dark-mood")
+}
+
+
